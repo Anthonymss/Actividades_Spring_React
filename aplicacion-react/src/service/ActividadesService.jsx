c49e72772f66bd4e46fd44beb1e65ed8a7d94232
Anthonyy Choque <giananthoych@outlook.es>
2024-08-09 19:51:03 -0500

    keep alive

--- aplicacion-react/src/service/ActividadesService.jsx
+++ aplicacion-react/src/service/ActividadesService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const urlbaseLocal="http://localhost:8080/api/v1";
-const urlHosting="https://actividades-spring-react.onrender.com/";
+const urlHosting="https://actividades-spring-react.onrender.com/api/v1";
 const URL=urlHosting+"/actividades";
 class ActividadesService{
 
